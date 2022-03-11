51ae92371e18bacceee8c636185c14d21e40fbb9
joycitta-siqueira <joycitta@gmail.com>
2022-03-10 22:54:29 -0300

    branch l3-start

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
     data(){
         return {
             product: 'Socks',
-            description: 'descricao de socks'
+            description: 'descricao de socks',
+            image: './assets/images/botaverde.jpg',
+            url: 'https://www.arezzo.com.br/sapatos/botas/bota-verde-croco-cano-alto-miranda-/p/1025304380001U?gclid=CjwKCAiA4KaRBhBdEiwAZi1zznX0L1B9aAomMFr6FqA-jPmPTYwt8YUgJBRAXYqK8hTohNapZ43V5hoCF_wQAvD_BwE'
         }
     }
 })
